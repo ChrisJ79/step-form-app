53ebca549e07db94905064f9fb16ff3bc7e55858
Chris <csjohnson79@icloud.com>
2020-02-12 19:15:35 -0700

    Built out FormUserDetails component, imported it into UserForm component, created AppBar

--- src/components/form-user-details.jsx
+++ src/components/form-user-details.jsx
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import '../App.css';
 
 class FormPersonalDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
     render() {
+        const {values, handleChange} = this.props;
         return (
-            <div>
-                 <h1>User Details</h1>
-            </div>
+            <MuiThemeProvider>
+                 <React.Fragment>
+                    <AppBar title="Enter User Details" align="center" />
+                    <TextField
+                        hintText="Enter Your First Name"
+                        floatingLabelText="First Name"
+                        onChange={handleChange('firstName')}
+                        defaultValue={values.firstName}
+                    />
+                    <br/>
+                    <TextField
+                        hintText="Enter Your Last Name"
+                        floatingLabelText="Last Name"
+                        onChange={handleChange('lastName')}
+                        defaultValue={values.lastName}
+                    />
+                 </React.Fragment>
+            </MuiThemeProvider>
         );
     }
 }
